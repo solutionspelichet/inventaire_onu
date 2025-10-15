4eb62fccd81a8d0d940cd940f9a157d3305ca39f
solutionspelichet <solutions.pelichet@gmail.com>
2025-10-15 12:59:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,3 @@
-// SW minimal pour installabilité (pas de cache de données)
-// Ne met en cache aucun appel API.
-
-self.addEventListener('install', (e) => {
-  // immédiat
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e) => {
-  // prise de contrôle
-  e.waitUntil(self.clients.claim());
-});
+// SW minimal pour installabilité (pas de cache API ni data)
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
